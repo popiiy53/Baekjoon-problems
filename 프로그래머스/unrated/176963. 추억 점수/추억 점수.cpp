1512cb0b5a15462781a0032adeb86cce64c69e93
sandeul2 <107553545+sandeul2@users.noreply.github.com>
2023-08-29 15:25:28 +0900

    [unrated] Title: 추억 점수, Time: 0.01 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
+++ 프로그래머스/unrated/176963. 추억 점수/추억 점수.cpp
@@ -1,26 +1,21 @@
 #include <string>
 #include <vector>
-#include <map>
+
 using namespace std;
 
 vector<int> solution(vector<string> name, vector<int> yearning, vector<vector<string>> photo) {
     vector<int> answer;
-    
-    map<string,int> m; 
-    
-    for(int i=0; i<name.size(); i++){
-        m.insert({name[i],yearning[i]});
-    }
-    
-    for(int i=0; i<photo.size();i++){
-        int sum=0;
-        for(int j=0; j<photo[i].size(); j++){
-            string person = photo[i][j];  
-            if(m.find(person)!=m.end()){//추억의 인물인 경우
-                sum += m[person];
+
+    for (int i=0;i<photo.size();i++){
+        int sum{0};
+        for (int j=0;j<photo[i].size();j++){
+            for (int k=0;k<name.size();k++){
+                if(photo[i][j] == name[k]){
+                    sum += yearning[k];
+                }
             }
         }
-        answer.push_back(sum);
+        answer.emplace_back(sum);
     }
     return answer;
 }
